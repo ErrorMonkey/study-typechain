0e7ac5836c6cea2cd66ff971d429d397856b76c6
ErrorMonkey <puts.hello.world.return0@gmail.com>
2024-02-11 23:40:10 +0900

    ts-study: 2

--- build/index.js
+++ build/index.js
@@ -1,8 +1,5 @@
-class Block {
-    constructor(data) {
-        this.data = data;
-    }
-    static hello() {
-        return "hi";
-    }
+class Dev {
+    constructor(name, language, os, companyName) { }
 }
+const devSeyun = new Dev("세윤", ["C#", "JAVA"], ["mac", "window"], "company");
+export {};
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,37 @@
-import { init, exit } from "myPackage";
+// import { init, exit } from "myPackage";
 
-init({
-  url: "true",
-});
+// interface DevProp extends Company {
+//   name: string;
+//   language: string[];
+//   os: string[];
+// }
 
-exit(1);
+// interface Company {
+//   companyName: string;
+// }
+
+// class Dev implements DevProp {
+//   constructor(name: string, language: string[], os: string[], companyName: string) {}
+// }
+
+// const devSeyun = new Dev("세윤", ["C#", "JAVA"], ["mac", "window"], "company");
+
+console.log("bye");
+
+import * as crypto from "crypto";
+
+interface BlockShape {
+  PrevHash: string;
+  heigth: number;
+  data: string;
+}
+
+class Block implements BlockShape {
+  public hash: string;
+  constructor(public prevHash: string, public height: string, public data: string) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+  }
+}
